8f88106988ed091e15e3d3fa849fc95c1f273a50
Pipe1098 <pipe1e@hotmail.com>
2023-02-02 22:33:14 -0500

    Continuacion del ejercicio

---
+++ Banco.java
@@ -0,0 +1,94 @@
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Banco {
+
+    public static void RealizarTransferencia(Cuenta c1, Cuenta c2, double Transferencia) {
+
+        Boolean x = c1.ValidarMonto(c1, Transferencia);
+        if (x) {
+            c2.AgregarNuevoSaldo(Transferencia);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        // Para la primera cuenta bancaria
+        System.out.println("Cuenta bancaria 1");
+        // Pedimos el Titular y el Saldo de la primera cuenta bancaria
+        // Lo almacenamos en un Array
+        String[] datos = pedirDatosUsuario();
+
+        // Instanciar la clase Cuenta y crear la clase Cuenta_1
+        Cuenta Cuenta_1 = new Cuenta(datos[0], Double.valueOf(datos[1]));
+
+        // Repetimos el mismo proceso para crear la Cuenta_2
+        // Para la segunda cuenta bancaria
+        System.out.println("\nCuenta bancaria 2");
+        datos = pedirDatosUsuario();
+        Cuenta Cuenta_2 = new Cuenta(datos[0], Double.valueOf(datos[1]));
+
+        // Hacemos una transferencia desde la Cuenta_1 a la Cuenta_2
+        Double importe = pedirImporteUsuario();
+        RealizarTransferencia(Cuenta_1, Cuenta_2, importe);
+
+        // Mostramos los datos de las cuentas bancarias por consola
+        System.out.println("\n*******************************************");
+        System.out.println("Datos de la Cuenta 1");
+        List<String> datosCuenta;
+        datosCuenta = Cuenta_1.getDatosCuenta();
+
+        System.out.println("   ->      Titular: " + datosCuenta.get(0));
+        System.out.println("   -> Nº de cuenta: " + datosCuenta.get(1));
+        System.out.println("   ->        Saldo: " + datosCuenta.get(2));
+
+        System.out.println("\nDatos de la Cuenta 2");
+        datosCuenta = Cuenta_2.getDatosCuenta();
+        System.out.println("   ->      Titular: " + datosCuenta.get(0));
+        System.out.println("   -> Nº de cuenta: " + datosCuenta.get(1));
+        System.out.println("   ->        Saldo: " + datosCuenta.get(2));
+        
+    }
+
+    
+    public static String[] pedirDatosUsuario() {
+        // Pedimos los datos al usuario por consola para la primera cuenta 
+
+        String[] arr = new String[2];
+        System.out.print("Introduzca el nombre del titular: ");
+        Scanner entradaEscaner = new Scanner(System.in); // Creamos un objeto Scanner
+        String titular = entradaEscaner.nextLine();
+
+        Double saldo = 0.0;
+        System.out.print("Introduzca el saldo inicial: ");
+        // Comprueba que sólo se introduzcan números válidos (Double)
+        // while (!entradaEscaner.hasNextDouble()) {
+        // System.out.print("No ha introducido un saldo correcto.\n" +
+        // "Vuelva a intentarlo (sólo números y decimales): ");
+        // entradaEscaner.nextLine();
+        // }
+
+        saldo = entradaEscaner.nextDouble();
+
+        arr[0] = titular;
+        arr[1] = saldo.toString();
+
+        return arr;
+
+    }
+
+    // Solicita el importe a transferir entre cuentas al usuario
+    public static Double pedirImporteUsuario() {
+        // Pedimos el importe al usuario por consola
+        System.out.print("\nIntroduzca el importe a transferir entre cuentas: ");
+        Scanner entradaEscaner = new Scanner(System.in);
+        double importe = entradaEscaner.nextDouble();// Creamos de un objeto Scanner
+
+        entradaEscaner.close();
+
+        return importe;
+
+    }
+
+}
--- Cuenta.java
+++ Cuenta.java
@@ -2,62 +2,82 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
- 
+
 class Cuenta {
-    //Propiedades de la clase Cuenta
+    // Propiedades de la clase Cuenta
     private double saldoDeCuenta;
     private String nombreDelTitular;
     private long numeroDeCuenta;
- 
-    //Constructor de la clase Cuenta
-    public Cuenta (String nombreDelTitular, double saldoDeCuenta)
-    {
+
+    // Constructor de la clase Cuenta
+    public Cuenta(String nombreDelTitular, double saldoDeCuenta) {
         this.saldoDeCuenta = saldoDeCuenta;
         this.nombreDelTitular = nombreDelTitular;
         Random numAleatorioCuenta = new Random();
         this.numeroDeCuenta = Math.abs(numAleatorioCuenta.nextLong());
     }
- 
-    //Setters
- 
-    //Setter para aumentar el saldo de la cuenta bancaria (hacer un ingreso)
-    public void setIngreso (double ingreso)
-    {
+
+    // Setters
+
+    // Setter para aumentar el saldo de la cuenta bancaria (hacer un ingreso)
+    public void setIngreso(double ingreso) {
         if (ingreso <= 0)
             System.out.println("El ingreso debe ser superior a cero pesos");
         else
             saldoDeCuenta = saldoDeCuenta + ingreso;
     }
- 
-    //Setter para disminuir  el saldo de la cuenta bancaria (sacar dinero)
-    public void setRetiro (double retiro)
-    {
+
+    // Setter para disminuir el saldo de la cuenta bancaria (sacar dinero)
+    public void setRetiro(double retiro) {
         if (retiro <= 0)
             System.out.println("El retiro debe ser superior a cero pesos");
         else
             saldoDeCuenta = saldoDeCuenta - retiro;
     }
- 
-    //Getters
-    
-    //Getter que devuelve el saldo actual de la cuenta bancaria
-    public Double getSaldoCuenta ()
-    {
+
+    // Getters
+
+    // Getter que devuelve el saldo actual de la cuenta bancaria
+    public Double getSaldoCuenta() {
         return saldoDeCuenta;
     }
- 
-    //Getter que obtiene todos los datos de la cuenta bancaria en un List
-    //Se puede usar cualquier otra opción para devolver los datos
-    public List<String> getDatosCuenta ()
-    {
+
+    // Getter que obtiene todos los datos de la cuenta bancaria en un List
+    // Se puede usar cualquier otra opción para devolver los datos
+    public List<String> getDatosCuenta() {
         List<String> datosCuenta = new ArrayList<>();
         datosCuenta.add(nombreDelTitular);
         datosCuenta.add(Long.toString(numeroDeCuenta));
-        //Redondeamos a dos decimales el saldo
+        // Redondeamos a dos decimales el saldo
         DecimalFormat saldoRedondeado = new DecimalFormat("###.##");
         datosCuenta.add(saldoRedondeado.format(saldoDeCuenta));
- 
+
         return datosCuenta;
     }
- 
+
+    // Métodos
+    /*
+     * Métodos que incrementa el saldo de una Cuenta de destino y lo decrementa en
+     * la
+     * Cuenta de origen y se valida que haya dinero suficiente en la cuenta de
+     * origen y que el monto de la transferencia sea valido
+     */
+    public void AgregarNuevoSaldo(double saldoAgregar) {
+        saldoDeCuenta = saldoDeCuenta + saldoAgregar;
+    }
+
+    public Boolean ValidarMonto(Cuenta c1,double monto) {
+        Boolean confirm=true;
+        double rest=saldoDeCuenta-monto;
+        if (rest>=0) {
+            saldoDeCuenta= saldoDeCuenta - monto;
+        }
+        else{
+            System.out.println("No tiene suficiente dinero para hacer la trasferencia");
+            confirm=false;
+        }
+        return confirm;
+        
+    }
+
 }
